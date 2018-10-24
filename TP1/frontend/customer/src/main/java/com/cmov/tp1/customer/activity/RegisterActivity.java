4963a996075c37c552163ffb112c0e6f5054f0ec
Diogo Duque <diogoduque13@hotmail.com>
2018-10-24 14:25:33 +0100

    merged credit card info to (more complete) register activity

--- TP1/frontend/customer/src/main/java/com/cmov/tp1/customer/activity/RegisterActivity.java
+++ TP1/frontend/customer/src/main/java/com/cmov/tp1/customer/activity/RegisterActivity.java
@@ -1,15 +1,25 @@
 package com.cmov.tp1.customer.activity;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.cmov.tp1.customer.R;
+import com.cmov.tp1.customer.utility.MonthYearPickerDialog;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class RegisterActivity extends AppCompatActivity {
+    private static final String TAG = "RegisterActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,21 +33,57 @@ public class RegisterActivity extends AppCompatActivity {
                 register();
             }
         });
+
+        final EditText cardDateInput = findViewById(R.id.card_validity_input);
+        cardDateInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pickCardDate();
+            }
+        });
     }
 
     private void register() {
-        EditText nameInput = findViewById(R.id.user_input);
+        EditText nameInput = findViewById(R.id.name_input);
         EditText usernameInput = findViewById(R.id.username_input);
         EditText nifInput = findViewById(R.id.nif_input);
         EditText passwordInput = findViewById(R.id.password_input);
+        EditText cardNumberInput = findViewById(R.id.card_number_input);
+        EditText cardCodeInput = findViewById(R.id.card_code_input);
+        EditText cardValidityInput = findViewById(R.id.card_validity_input);
+        RadioGroup cardTypeGroup = findViewById(R.id.card_type_group);
+        RadioButton cardTypeSelected = findViewById(cardTypeGroup.getCheckedRadioButtonId());
+
         String name = nameInput.getText().toString();
         String username = usernameInput.getText().toString();
         String nif = nifInput.getText().toString();
         String password = passwordInput.getText().toString();
-        if(name.length() == 0 || username.length()==0 || nif.length() != 9 || password.length()==0) {
-            Toast.makeText(this, "There are empty fields yet.", Toast.LENGTH_SHORT).show();
+        String cardNumber = cardNumberInput.getText().toString();
+        String cardCode = cardCodeInput.getText().toString();
+        String cardValidity = cardValidityInput.getText().toString();
+        String cardType = cardTypeSelected.getText().toString();
+        if(name.length() == 0 || username.length()==0 || nif.length() != 9 || password.length()==0 ||
+                cardNumber.length() != 16 || cardCode.length() != 3 || cardValidity.length()==0 || cardType.length() == 0) {
+            Toast.makeText(this, "There are empty/incorrect fields.", Toast.LENGTH_SHORT).show();
             return;
         }
-        //TODO credit card stuff
+        //TODO make request
+    }
+
+    private void pickCardDate() {
+        final EditText cardDateInput = findViewById(R.id.card_validity_input);
+        final Calendar calendar = Calendar.getInstance();
+        MonthYearPickerDialog pd = new MonthYearPickerDialog();
+        pd.setListener(new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, month-1);
+                // update
+                //cardDateInput.setText(month+);
+                cardDateInput.setText(new SimpleDateFormat("MM/yy", Locale.UK).format(calendar.getTime()));
+            }
+        });
+        pd.show(getSupportFragmentManager(), "MonthYearPickerDialog");
     }
 }
