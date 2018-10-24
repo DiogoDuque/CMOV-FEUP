9d153ce7a0f0052ed7291d4c22559c68eb4f5ba8
Diogo Duque <diogoduque13@hotmail.com>
2018-10-24 22:41:49 +0100

    boilerplate auth (passport) code

--- TP1/backend/server/src/routes/authRoute.js
+++ TP1/backend/server/src/routes/authRoute.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const passport = require('passport');
 
 const router = express.Router();
 
-router.post('/login', (req, res) => {
+// Just for checking if logged in
+router.get('/', (req, res) => {
+    if(req.isAuthenticated()) {
+      res.status(200).send();
+    } else res.status(401).send();
+  });
+
+router.post('/login', passport.authenticate('local'), (req, res) => {
     console.log(JSON.stringify(req.body));
     console.log('Cannot login (yet)');
     res.status(501).send();
