008b03d102982d754ee966ba636caee1bc22a3f5
Sara Couto <saracouto1318@hotmail.com>
2018-10-28 13:04:44 +0000

    customer activities created

---
+++ TP1/frontend/customer/src/main/java/com/cmov/tp1/customer/activity/OrderFinishedActivity.java
@@ -0,0 +1,15 @@
+package com.cmov.tp1.customer.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.cmov.tp1.customer.R;
+
+public class OrderFinishedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
