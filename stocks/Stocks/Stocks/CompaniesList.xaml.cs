a6f99eb30dc218b172651adc2c0d35073e3e54dd
Sara Couto <saracouto1318@hotmail.com>
2018-11-24 16:23:00 +0000

    add pages to project

---
+++ stocks/Stocks/Stocks/CompaniesList.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Stocks
+{
+    public partial class CompaniesList : ContentPage
+    {
+        public CompaniesList()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ stocks/Stocks/Stocks/ListCompanies.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Stocks
+{
+    public class ListCompanies : ContentPage
+    {
+        public ListCompanies()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
