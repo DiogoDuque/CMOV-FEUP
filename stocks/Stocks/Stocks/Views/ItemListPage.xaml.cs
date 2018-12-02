f231997cbef3d55466c69d8c80f58ca1583dadb0
Sara Couto <saracouto1318@hotmail.com>
2018-12-02 11:33:29 +0000

    some changes

--- stocks/Stocks/Stocks/Views/ItemListPage.xaml.cs
+++ stocks/Stocks/Stocks/Views/ItemListPage.xaml.cs
@@ -38,9 +38,29 @@ namespace Stocks.Views
             foreach(Company company in itemListViewModel.Companies){
                 if (company.Name == name){
                     if (!companies.Contains(selectedFromList) && companies.Count < 2)
+                    {
                         companies.Add(company);
+
+                        selectLabel.IsVisible = true;
+                        selectedCompanies.IsVisible = true;
+
+                        if (companies.Count == 1)
+                            selectedCompanies.Text += company.Name;
+                        else
+                            selectedCompanies.Text += " and " + company.Name;
+                    }
                     else if (companies.Contains(selectedFromList))
+                    {
                         companies.Remove(company);
+                        if (companies.Count == 0)
+                        {
+                            selectedCompanies.Text = "";
+                            selectedCompanies.IsVisible = false;
+                            selectLabel.IsVisible = true;
+                        }
+                        else
+                            selectedCompanies.Text += companies[0].Name;
+                    }
                     else
                         DisplayAlert("Alert", "You have to select only 1 or 2 companies", "OK");
                 }
