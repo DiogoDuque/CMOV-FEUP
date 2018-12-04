830cf38913982a90006e08274bd7136f8b7c6fa8
Sara Couto <saracouto1318@hotmail.com>
2018-12-04 18:28:21 +0000

    change ip interface done

---
+++ stocks/Stocks/Stocks/Views/ChangeIP.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Stocks.Views
+{
+    public partial class ChangeIP : ContentPage
+    {
+        public ChangeIP()
+        {
+            InitializeComponent();
+
+            saveButton.Clicked += SaveButton_Clicked;
+        }
+
+        void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            String ip = IP.Text;
+
+            if (ip.Length != 0)
+            {
+                Navigation.PushModalAsync(new ItemListPage());
+            }
+            else
+                DisplayAlert("Alert", "You have to write your server's IP address", "OK");
+        }
+
+    }
+}
